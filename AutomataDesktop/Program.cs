53e870e52b12a32f21da917b83a10f6142580df3
ayykapu <kurkeu@gmail.com>
2024-12-17 09:29:44 +0100

    Dodaj pliki projektów.

---
+++ AutomataDesktop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AutomataDesktop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
