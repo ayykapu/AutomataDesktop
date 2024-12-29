3a4caca33ab33f53e91442fa7dc40554e1f5f04e
ayykapu <kurkeu@gmail.com>
2024-12-30 00:05:19 +0100

    #

---
+++ AutomataDesktop/Automata.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomataDesktop
+{
+    public class Automata
+    {
+        public int _generationWidth;
+        public int _generationHeight;
+        RuleSet _ruleSet;
+        public List<byte[,]> _sequence = new List<byte[,]>();
+
+        public Automata(int width, int height, ValueTuple<int, int, byte>[] vectors, RuleSet ruleSet)
+        {
+            _generationWidth = width;
+            _generationHeight = height;
+
+            _ruleSet = ruleSet;
+
+            _sequence.Add(SetGenerationZero(vectors));
+        }
+
+        byte[,] SetGenerationZero(ValueTuple<int, int, byte>[] vectors)
+        {
+            byte[,] result = new byte[_generationHeight, _generationWidth];
+
+            foreach (var vector in vectors) 
+            {
+                result[vector.Item1, vector.Item2] = vector.Item3;
+            }
+
+            return result;
+        }
+
+        public void AddNextGeneration()
+        {
+            switch (_ruleSet)
+            {
+                case RuleSet.FALLINGSAND:
+                    _sequence.Add(FallingSandRule(_sequence[_sequence.Count - 1]));
+                    break;
+                case RuleSet.GAMEOFLIFE:
+                    //_sequence.Add(GameOfLifeRule(_sequence[_sequence.Count - 1]));
+                    break;
+            }
+        }
+        byte[,] FallingSandRule(byte[,] generation)
+        {
+            byte[,] result = new byte[_generationHeight, _generationWidth];
+
+            for (int i = 0; i < _generationHeight; i++)
+            {
+                for (int j = 0; j < _generationWidth; j++)
+                {
+                    byte thisCellState = generation[i, j];
+
+                    byte topState, bottomState;
+
+                    if (i == 0)
+                    {
+                        topState = generation[_generationHeight - 1, j];
+                    } 
+                    else
+                    {
+                        topState = generation[i - 1, j];
+                    }
+
+                    if (i == _generationHeight - 1)
+                    {
+                        bottomState = generation[0, j];
+                    }
+                    else
+                    {
+                        bottomState = generation[i + 1, j];
+                    }
+
+                    if (i == 0)
+                    {
+                        result[i, j] = 0;
+                    }
+                    else if (i > 0 && i < _generationHeight - 1)
+                    {
+                        if (topState == 1 && thisCellState == 0 || bottomState == 1 && thisCellState == 1)
+                        {
+                            result[i, j] = 1;
+                        }
+                        else
+                        {
+                            result[i, j] = 0;
+                        }
+                    }
+                    else
+                    {
+                        if (topState == 1 || thisCellState == 1)
+                        {
+                            result[i, j] = 1;
+                        }
+                        else
+                        {
+                            result[i, j] = 0;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public enum RuleSet
+        {
+            FALLINGSAND,
+            GAMEOFLIFE,
+        }
+    }
+}
--- AutomataDesktop/Window.cs
+++ AutomataDesktop/Window.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Runtime.CompilerServices;
-using OpenTK.Graphics.OpenGL4;
-using OpenTK.Mathematics;
-using OpenTK.Platform.Windows;
+﻿using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
@@ -16,20 +12,28 @@ namespace AutomataDesktop
         private int _vao;
         private int _vbo;
 
+        static Automata _automata;
+        static int _displayIndex = 0;
+        static int[] _currentGeneration;
+
         static int _cellSize;
-        static List<byte[,]> _generationList;
-        static int[] _currentGeneration; //this is where non 0s goes
 
-        static int currentGenerationIndex = 1;
-
-        public Window(int width, int height, string title, List<byte[,]> generations) : base(GameWindowSettings.Default, new NativeWindowSettings() { ClientSize = (width, height), Title = title })
+        public Window(int width, int height, string title, Automata automata) : base(GameWindowSettings.Default, new NativeWindowSettings() { ClientSize = (width, height), Title = title })
         {
             _width = width;
             _height = height;
-            _generationList = generations;
+            _automata = automata;
 
-            SetWindowIcon("icon.ico"); // Set the icon file path
+            SetWindowIcon("icon.ico");
             CenterWindow();
+        }
+        private void SetWindowIcon(string iconPath)
+        {
+            //todo
+        }
+        private void SetDefaultCellsSize()
+        {
+            _cellSize = _width / _automata._generationWidth;
         }
         private static int[] TranslateBytesIntoInts(byte[,] generation)
         {
@@ -69,15 +73,6 @@ namespace AutomataDesktop
 
             return ndcCoordinates;
         }
-        private void SetWindowIcon(string iconPath)
-        {
-            //todo
-        }
-        private void SetDefaultCellsSize()
-        {
-            int generationWidth = _generationList[0].GetLength(1);
-            _cellSize = _width / generationWidth;
-        }
         protected override void OnLoad()
         {
             base.OnLoad();
@@ -100,47 +95,50 @@ namespace AutomataDesktop
 
             if (MouseState.IsButtonDown(MouseButton.Left))
             {
-                if (currentGenerationIndex < _generationList.Count - 1)
+                if (_displayIndex < _automata._sequence.Count - 1)
                 {
-                    currentGenerationIndex++;
-                    Console.WriteLine(currentGenerationIndex);
+                    _displayIndex++;
+                    Console.WriteLine(_displayIndex);
                 }
             }
 
             if (MouseState.IsButtonDown(MouseButton.Right))
             {
-                if (currentGenerationIndex > 0)
+                if (_displayIndex > 0)
                 {
-                    currentGenerationIndex--;
-                    Console.WriteLine(currentGenerationIndex);
+                    _displayIndex--;
+                    Console.WriteLine(_displayIndex);
                 }
             }
 
             if (KeyboardState.IsKeyReleased(Keys.Right))
             {
-                if (currentGenerationIndex < _generationList.Count - 1)
+                if (_displayIndex < _automata._sequence.Count - 1)
                 {
-                    currentGenerationIndex++;
-                    Console.WriteLine(currentGenerationIndex);
+                    _displayIndex++;
+                    Console.WriteLine(_displayIndex);
                 }
             }
 
             if (KeyboardState.IsKeyReleased(Keys.Left))
             {
-                if (currentGenerationIndex > 0)
+                if (_displayIndex > 0)
                 {
-                    currentGenerationIndex--;
-                    Console.WriteLine(currentGenerationIndex);
+                    _displayIndex--;
+                    Console.WriteLine(_displayIndex);
                 }
             }
 
-
-            _currentGeneration = TranslateBytesIntoInts(_generationList[currentGenerationIndex]);
+            _currentGeneration = TranslateBytesIntoInts(_automata._sequence[_displayIndex]);
             float[] vertices = ConvertToNDC(_currentGeneration, _width, _height);
 
             GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
 
             GL.VertexAttribPointer(0, 2, VertexAttribPointerType.Float, false, 2 * sizeof(float), 0);
+
+            _automata.AddNextGeneration();
+            Console.WriteLine(_automata._sequence.Count);
+
             GL.EnableVertexAttribArray(0);
 
             GL.BindVertexArray(0);
